afd67c561479f0079f35919993a9621ba387f1a9
rioanugrah <50478866+rioanugrah@users.noreply.github.com>
2019-05-10 14:39:42 +0700

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Platform, Dimensions} from 'react-native';
+import {createDrawerNavigator, createAppContainer, createBottomTabNavigator} from 'react-navigation';
+import HomePage from '../src/HomePage';
+import ProfilePage from '../src/ProfilePage';
+import LinkScreen from '../src/LinkScreen';
+import SettingScreen from '../src/SettingScreen';
+import MenuDrawer from '../components/MenuDrawer';
+
+const WIDTH = Dimensions.get('window').width;
+
+const DrawerConfig = {
+    drawerWidth: WIDTH*0.83,
+    contentComponent: ({navigation}) => {
+        return(<MenuDrawer navigation={navigation}/>)
+    }
+}
+const TabNavigasi = createBottomTabNavigator(
+    {
+      Home:HomePage,
+      Profile:ProfilePage,
+    },
+    {initialRouteName:'Home'}
+  )
+  
+const DrawerNavigator = createDrawerNavigator(
+    {
+    Home:{
+        screen:TabNavigasi
+        },
+    Links:{
+        screen:LinkScreen
+        },
+    SettingScreen:{
+        screen:SettingScreen
+        },
+    },
+    DrawerConfig
+);
+export default createAppContainer(DrawerNavigator);
