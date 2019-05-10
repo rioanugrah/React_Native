f4b567eaa5966876eff188d096922feff2e83ea7
rioanugrah <50478866+rioanugrah@users.noreply.github.com>
2019-05-10 14:32:45 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Login from './src/Login';
+import HomePage from './src/HomePage';
+import DrawerNavigator from './navigation/DrawerNavigator';
+import {createSwitchNavigator,createStackNavigator,createBottomTabNavigator,createAppContainer, createDrawerNavigator} from "react-navigation";
+import LinkScreen from './src/LinkScreen';
+import SettingScreen from './src/SettingScreen';
+
+const LoginNavigasi = createSwitchNavigator(
+  {
+    Login:Login,
+  }
+)
+const Navigasi = createSwitchNavigator(
+  {
+    Login:LoginNavigasi,
+    Drawer:DrawerNavigator,
+  },
+  {initialRouteName:'Login'}
+)
+const AppContainer = createAppContainer(Navigasi);
+export default class App extends React.Component {
+  render() {
+    return (
+      // <HomePage/>
+      <AppContainer/>
+      // <DrawerNavigator/>
+    );
+  }
+}
