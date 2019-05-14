49a56580b02950b7f51998a6779b0110d8a22118
rioanugrah <50478866+rioanugrah@users.noreply.github.com>
2019-05-14 20:02:40 +0700

    Update ProfilePage.js

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
-
+import MenuButton from '../components/MenuButton'
 export default class ProfilePage extends Component {
     _goToHome=()=>{
         this.props.navigation.navigate('Home')
@@ -14,6 +14,7 @@ export default class ProfilePage extends Component {
     render() {
       return (
         <View style={styles.container}>
+          <MenuButton navigation={this.props.navigation}/>
           <Text> This is your Profile </Text>
               <View style={styles.button}>
               <Button
