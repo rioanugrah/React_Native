53f1900c8e1eb9a17f38f76c2f9a06dde0e3d6fe
rioanugrah <50478866+rioanugrah@users.noreply.github.com>
2019-06-19 10:59:33 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import DrawerNavigator from './navigation/DrawerNavigator';
 import {createSwitchNavigator,createStackNavigator,createBottomTabNavigator,createAppContainer, createDrawerNavigator} from "react-navigation";
 import LinkScreen from './src/LinkScreen';
 import SettingScreen from './src/SettingScreen';
-
+import firebase from "firebase";
+import AuthLoadingScreen from './screen/AuthLoadingScreen';
 const LoginNavigasi = createSwitchNavigator(
   {
     Login:Login,
@@ -15,16 +16,28 @@ const Navigasi = createSwitchNavigator(
   {
     Login:LoginNavigasi,
     Drawer:DrawerNavigator,
+    AuthLoading:AuthLoadingScreen,
   },
   {initialRouteName:'Login'}
 )
 const AppContainer = createAppContainer(Navigasi);
 export default class App extends React.Component {
+  componentWillMount(){
+    firebase.initializeApp({
+    apiKey: "AIzaSyA19yztv_OQRKAwGOEVVBR6for7rpwI9hM",
+    authDomain: "expo-575a3.firebaseapp.com",
+    databaseURL: "https://expo-575a3.firebaseio.com",
+    projectId: "expo-575a3",
+    storageBucket: "expo-575a3.appspot.com",
+    messagingSenderId: "578239107125",
+    appId: "1:578239107125:web:e17d6b9169819c37"
+    })
+  }
   render() {
     return (
       // <HomePage/>
-      // <AppContainer/>
-      <DrawerNavigator/>
+      <AppContainer/>
+      // <DrawerNavigator/>
     );
   }
 }
